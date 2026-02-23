9d9e1d7b9b8c927bc310d69d1e7027ffedccb2df
Hallvar Haugdal <hallvar.haugdal@sintef.no>
2026-02-23 12:43:47 +0100

    Updated simulation in N44 example

--- examples/nordic44_rtsim/sim.py
+++ examples/nordic44_rtsim/sim.py
@@ -4,11 +4,27 @@ import tops.dynamic as dps
 
 import multiprocessing as mp
 from topsrt_random_load_variations import RandomLoadVariations, remove_model_data
-import n44_model as model_data
+# import n44_model as model_data
+import json
+from pathlib import Path
 
 def create_sim():
 
-    model = model_data.load()
+    with open(str(Path(__file__).parent/'data/model_data.json')) as f:
+        data = f.read()
+    model = json.loads(data)
+    model["buses"] = model.pop("bus")
+    model["lines"] = model.pop("line")
+    model["trafos"] = model.pop("trafo")
+    model["loads"] = model.pop("load")
+    keys = list(model.keys())
+    for key in keys:
+        spl = key.split(":")
+        if len(spl) <= 1:
+            continue
+        model[spl[0]] = {spl[1]: model.pop(key)}    
+
+    # model = model_data.load()
     rows = []
     for target_load in ['L5610-1']:  # , 'L6700-mod']:
         target_load_idx = np.argwhere([row[0] == target_load for row in model['loads']])[0][0]
@@ -46,3 +62,8 @@ def create_sim():
 
     # ps.ode_fun(0, ps.x0)
     return ps
+
+if __name__ == "__main__":
+    ps = create_sim()
+    ps.init_dyn_sim()
+    print(max(abs(ps.state_derivatives(0, ps.x0, ps.v0))))
