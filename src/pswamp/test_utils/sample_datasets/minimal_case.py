b7662de243c680ffe5e6c92fadc66572dce00519
Hallvar Haugdal <hallvar.haugdal@sintef.no>
2026-04-07 15:09:31 +0200

    Changed license header to short SPDX identifier

--- src/pswamp/test_utils/sample_datasets/minimal_case.py
+++ src/pswamp/test_utils/sample_datasets/minimal_case.py
@@ -1,6 +1,6 @@
 # LICENSE HEADER MANAGED BY add-license-header
 #
-# Copyright 2026 NTNU/SINTEF/Statnett SF
+# Copyright 2026 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# Copyright Contributors to the p-SWAMP Project.
 
 from synchrophasor.simplePMU import SimplePMU
 from pswamp.test_utils import runners
