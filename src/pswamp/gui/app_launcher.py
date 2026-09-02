c45def29b50fd3d8fcf23998fa22013fb32f4c48
Hallvar Haugdal <hallvar.haugdal@sintef.no>
2026-09-02 20:34:09 +0200

    fix: removed voltage phasor plot from app launcher, since this does not work
    Signed-off-by: Hallvar Haugdal <hallvar.haugdal@sintef.no>

--- src/pswamp/gui/app_launcher.py
+++ src/pswamp/gui/app_launcher.py
@@ -145,18 +145,18 @@ class AppLauncher(QWidget):
         # button.clicked.connect(clicked_fun)
         # layout.addWidget(button, 3, 1)
 
-        button = QPushButton('Voltage phasor plot')
-        button.setToolTip(
-            '''Visualization: A phasor plot showing the voltage phasors in the system.''')
+        # button = QPushButton('Voltage phasor plot')
+        # button.setToolTip(
+        #     '''Visualization: A phasor plot showing the voltage phasors in the system.''')
 
-        def clicked_fun():
-            print('Launching Voltage phasor plot')
-            # fft.main(ip, port)
-            self.p = mp.Process(target=run_voltage_phasor_plot, args=(config,))
-            self.p.start()
-            # p.join()
-        button.clicked.connect(clicked_fun)
-        layout.addWidget(button, 3, 1)
+        # def clicked_fun():
+        #     print('Launching Voltage phasor plot')
+        #     # fft.main(ip, port)
+        #     self.p = mp.Process(target=run_voltage_phasor_plot, args=(config,))
+        #     self.p.start()
+        #     # p.join()
+        # button.clicked.connect(clicked_fun)
+        # layout.addWidget(button, 3, 1)
 
         button = QPushButton('Islanding detection')
         button.setToolTip(
