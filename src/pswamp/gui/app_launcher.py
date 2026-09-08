6483405aef5fdb4b1bc4bb7f399c4e98ff6d657c
Hallvar Haugdal <hallvar.haugdal@sintef.no>
2026-09-08 09:27:01 +0200

    fix: removed frequency heat map button, since this currently does not work
    Signed-off-by: Hallvar Haugdal <hallvar.haugdal@sintef.no>

--- src/pswamp/gui/app_launcher.py
+++ src/pswamp/gui/app_launcher.py
@@ -122,18 +122,18 @@ class AppLauncher(QWidget):
         button.clicked.connect(clicked_fun)
         layout.addWidget(button, 2, 1)
 
-        button = QPushButton('Frequency heatmap')
-        button.setToolTip(
-            '''Visualization: A heat map indicating frequency variations throughout the system.''')
+        # button = QPushButton('Frequency heatmap')
+        # button.setToolTip(
+        #     '''Visualization: A heat map indicating frequency variations throughout the system.''')
 
-        def clicked_fun():
-            print('Launching Frequency heatmap')
-            # fft.main(ip, port)
-            self.p = mp.Process(target=run_freq_heatmap, args=(config,))
-            self.p.start()
-            # p.join()
-        button.clicked.connect(clicked_fun)
-        layout.addWidget(button, 3, 0)
+        # def clicked_fun():
+        #     print('Launching Frequency heatmap')
+        #     # fft.main(ip, port)
+        #     self.p = mp.Process(target=run_freq_heatmap, args=(config,))
+        #     self.p.start()
+        #     # p.join()
+        # button.clicked.connect(clicked_fun)
+        # layout.addWidget(button, 3, 0)
 
         # button = QPushButton('Voltage heatmap')
         # def clicked_fun():
